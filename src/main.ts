d71b86a5a036a0e9bec7b6228619ec24f435fd9f
Sesaque Cruz <cruz.sesaque@gmail.com>
2025-07-09 22:21:45 -0300

    chore: add main to bootstrap the application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+import { version } from '../package.json';
+import { EnvDto } from './infra/config/env.dto';
+import { MainModule } from './main.module';
+
+async function main(): Promise<void> {
+  const app = await NestFactory.create(MainModule);
+
+  const configService = app.get<ConfigService<EnvDto, true>>(ConfigService);
+
+  app.setGlobalPrefix(configService.get('API_URL'));
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('My Finance Web - API')
+    .setVersion(version)
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+
+  SwaggerModule.setup(
+    `${configService.get('API_URL')}/swagger`,
+    app,
+    swaggerDocument,
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(configService.get('API_PORT'));
 }
-bootstrap();
+
+void main();
