e1075beb5e9278bd1d0c8b1dc8178566d69cdbd6
Sesaque Cruz <cruz.sesaque@gmail.com>
2025-07-11 23:50:02 -0300

    test: add tests for transaction repository

---
+++ test/repositories/generators/transaction.repository.generator.ts
@@ -0,0 +1,15 @@
+import { faker } from '@faker-js/faker';
+import { TransactionEntity } from 'src/domain/entities/transaction.entity';
+
+export function createTransactionEntity(): TransactionEntity {
+  return {
+    id: faker.number.int(),
+    history: faker.word.words(),
+    date: faker.date.recent(),
+    accountId: faker.number.int(),
+    value: faker.number.float(),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent(),
+    deletedAt: null,
+  };
+}
